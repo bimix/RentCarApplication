5415f11443a5a8db3caf76e220ca3ba9308aa536
BurimSyla <bimi.syla@hotmail.com>
2018-10-14 18:44:29 +0200

    testing methods added

---
+++ RentCarApplication.Tests/Controllers/CarsControllerTests.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RentCarApplication.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http;
+using System.Web.Http;
+using CarDataAccess;
+using System.Web.Http.Hosting;
+using System.Net;
+using System.Web.Http.Results;
+using System.Web.Http.Routing;
+
+namespace RentCarApplication.Controllers.Tests
+{
+    [TestClass()]
+    public class CarsControllerTests
+    {
+        //[TestMethod()]
+        //public void GetTest()
+        //{
+        //    var controller = new CarsController();
+        //    controller.Request = new HttpRequestMessage();
+        //    controller.Configuration = new HttpConfiguration();
+
+        //    var response = controller.Get(5);
+
+        //    Car car;
+        //    Assert.IsTrue(response.TryGetContentValue<Car>(out car));
+        //    Assert.AreEqual("5", car.Id);
+        //}
+
+
+
+        [TestMethod()]
+        public void GetAllCarsTest()
+        {
+            var controller = new CarsController();
+
+            var cars = controller.Get();
+
+            Assert.AreEqual(8, cars.Count());
+        }
+
+
+
+
+        [TestMethod()]
+        public void GettingaCarWithKnownID()
+        {
+            var controller = new CarsController
+            {
+                Request = new HttpRequestMessage()
+                {
+                    Properties = { { HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration() } }
+                }
+            };
+
+            var response = controller.Get(2);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var car = response.Content.ReadAsAsync<Car>().Result;
+            Assert.AreEqual(2, car.Id);
+        }
+
+
+        [TestMethod()]
+        public void GettingaCarWithUnknownID()
+        {
+            var controller = new CarsController()
+            {
+                Request = new HttpRequestMessage()
+                {
+                    Properties = { {
+                            HttpPropertyKeys.HttpConfigurationKey,
+                            new HttpConfiguration() } }
+
+                }
+            };
+
+            var response = controller.Get(111);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+
+        }
+
+
+
+
+        [TestMethod]
+        public void PostTest()
+        {
+            var httpconfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpconfiguration);
+            var httpRouteData = new HttpRouteData(httpconfiguration.Routes["DefaultApi"],
+                new HttpRouteValueDictionary
+                {
+                    { "controller", "Cars" }
+                });
+            var controller = new CarsController()
+            {
+                Request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:6664/api/cars/")
+                {
+                    Properties =
+                    {
+                        {
+                            HttpPropertyKeys.HttpConfigurationKey,httpconfiguration
+                        },
+                        {
+                            HttpPropertyKeys.HttpRouteDataKey, httpRouteData
+                        },
+
+                    }
+                }
+            };
+            var response = controller.Post(new Car()
+            {
+                Id = 223,
+                model = "Toyosdt",
+                make = "calidfgba",
+                location = "anylocation",
+                price = "4000",
+                avaEnd = null,
+                avaStart = null
+            });
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            var addedCar = response.Content.ReadAsAsync<Car>().Result;
+
+            Assert.AreEqual(string.Format("http://localhost:6664/api/cars/{0}",
+                addedCar.Id),
+                response.Headers.Location.ToString());
+
+        }
+
+
+
+
+        [TestMethod]
+        public void GetCarNotFound()
+        {
+            var controller = new CarsController();
+            HttpResponseMessage actionResult = controller.Get(2);
+
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
+        }
+
+
+
+        [TestMethod]
+        public void PutTest()
+        {
+            var controller = new CarsController()
+            {
+                Request = new HttpRequestMessage()
+                {
+                    Properties = { { HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration() } }
+
+                }
+            };
+
+            var car = new Car() { Id = 1, model = "new model", make = "new make" };
+            var response = controller.Put(car.Id, car);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var newCar = response.Content.ReadAsAsync<Car>().Result;
+            Assert.AreEqual("new model", newCar.model);
+            Assert.AreEqual("new make", newCar.make);
+
+        }
+    }
+}
--- RentCarApplication/Controllers/CarsController.cs
+++ RentCarApplication/Controllers/CarsController.cs
@@ -12,7 +12,7 @@ namespace RentCarApplication.Controllers
     {
         public IEnumerable<Car> Get()
         {
-            using(BookCarDBEntities entities = new BookCarDBEntities ())
+            using (BookCarDBEntities entities = new BookCarDBEntities())
             {
                 return entities.Cars.ToList();
             }
@@ -20,22 +20,116 @@ namespace RentCarApplication.Controllers
 
 
 
-        public Car Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (BookCarDBEntities entities = new BookCarDBEntities())
             {
-                return entities.Cars.FirstOrDefault(c => c.Id == id);
+                var entity = entities.Cars.FirstOrDefault(c => c.Id == id);
+
+                if (entities != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Car with id = " + id.ToString() + "was not found");
+                }
             }
         }
 
 
-        public void Post([FromBody] Car car)
+        public HttpResponseMessage Post([FromBody] Car car)
+        {
+            try
+            {
+
+                using (BookCarDBEntities entities = new BookCarDBEntities())
+                {
+                    entities.Cars.Add(car);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, car);
+                    message.Headers.Location = new Uri(Request.RequestUri + car.Id.ToString());
+                    return message;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+          
+                using (BookCarDBEntities entities = new BookCarDBEntities())
+                {
+                    var entity = entities.Cars.FirstOrDefault(e => e.Id == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Car with id = " + id.ToString() + "doesnt exists");
+                    }
+                    else
+                    {
+                        entities.Cars.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, "Car with id" + id + "has been deleted succesfully");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Get(object p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpResponseMessage Put (int id, [FromBody] Car car)
         {
             using (BookCarDBEntities entities = new BookCarDBEntities())
             {
-                entities.Cars.Add(car);
-                entities.SaveChanges();
+                try
+                {
+                    var entity = entities.Cars.FirstOrDefault(e => e.Id == id);
+
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Car with id =" + id.ToString() + "was not found!");
+
+                    }
+                    else
+                    {
+                        entity.model = car.model;
+                        entity.make = car.make;
+                        entity.location = car.location;
+                        entity.avaStart = car.avaStart;
+                        entity.avaEnd = car.avaEnd;
+                        entity.price = car.price;
+
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+                
+
+                
+
             }
+            
         }
     }
 }
